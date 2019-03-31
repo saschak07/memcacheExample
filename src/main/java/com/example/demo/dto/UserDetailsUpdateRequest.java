0fc1396aca73f136403f94c69ec0cc6cc1291e9b
Bishakha <bishakhacool@gmail.com>
2019-03-31 12:02:11 +0530

    adding logic to update cache

---
+++ src/main/java/com/example/demo/dto/UserDetailsUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+public class UserDetailsUpdateRequest {
+
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.UserDetailsUpdateRequest;
 import com.example.demo.entity.UserDetails;
 import com.example.demo.repository.UserDataRepository;
 @Service 
@@ -21,10 +23,20 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Override
-	@Cacheable
+	@Cacheable(value = "users", key = "#userName")
 	public Optional<UserDetails> getUserDetails(String userName) {
 		logger.info("reading details from database for user::"+userName);
 		Optional<UserDetails> optionalUserDetails = userRepositiry.findByName(userName);
+		return optionalUserDetails;
+	}
+
+	@Override
+	@CachePut(value = "users", key = "#userName")
+	public Optional<UserDetails> updateUserDetails(String userName, UserDetailsUpdateRequest userUpdateRequest) {
+		Optional<UserDetails> optionalUserDetails = userRepositiry.findByName(userName);
+		optionalUserDetails.get().setAddress(userUpdateRequest.getAddress());
+		logger.info("updating user address for user::"+userName);
+		userRepositiry.save(optionalUserDetails.get());
 		return optionalUserDetails;
 	}
 
